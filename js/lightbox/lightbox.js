b84036cbc2ec52115f60b9b897cb51ad9cb901e8
nihen99 <nick_henning@web.de>
2025-08-31 13:53:16 +0200

    JS fixes

--- js/lightbox/lightbox.js
+++ js/lightbox/lightbox.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
-    $(document).ready(function () {
-        $('.js-lightbox-trigger').click(function () {
-            const imgSrc = $(this).attr('src');
-            $('.c-lightbox__img').attr('src', imgSrc);
-            $('.c-lightbox').fadeIn();
-        });
-
-        $('.c-lightbox__close, .c-lightbox').click(function (e) {
-            // Nur schließen, wenn nicht direkt aufs Bild geklickt wurde
-            if (!$(e.target).is('.c-lightbox__img')) {
-                $('.c-lightbox').fadeOut();
-            }
-        });
+    $('.js-lightbox-trigger').click(function () {
+        const imgSrc = $(this).attr('src');
+        $('.c-lightbox__img').attr('src', imgSrc);
+        $('.c-lightbox').fadeIn();
     });
 
+    $('.c-lightbox__close, .c-lightbox').click(function (e) {
+        // Nur schließen, wenn nicht direkt aufs Bild geklickt wurde
+        if (!$(e.target).is('.c-lightbox__img')) {
+            $('.c-lightbox').fadeOut();
+        }
+    });
 });
