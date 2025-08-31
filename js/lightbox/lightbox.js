abe29df32c4d5aa314afc8e9ae8e9e3ff2630073
nihen99 <nick_henning@web.de>
2025-08-31 14:34:05 +0200

    Lightbox Navigation implementieren

--- js/lightbox/lightbox.js
+++ js/lightbox/lightbox.js
@@ -1,14 +1,29 @@
 $(document).ready(function () {
-    $('.js-lightbox-trigger').click(function () {
+    const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
+    let currentIndex = -1;
+
+    $galleryItems.click(function () {
+        currentIndex = $galleryItems.index(this);
         const imgSrc = $(this).attr('src');
         $('.c-lightbox__img').attr('src', imgSrc);
-        $('.c-lightbox').fadeIn().css( "display", "flex" );
+        $('.c-lightbox').fadeIn().css("display", "flex");
     });
 
     $('.c-lightbox__close, .c-lightbox').click(function (e) {
-        // Nur schließen, wenn nicht direkt aufs Bild geklickt wurde
-        if (!$(e.target).is('.c-lightbox__img')) {
+        if (!$(e.target).is('.c-lightbox__img') && !$(e.target).hasClass('c-lightbox__arrow')) {
             $('.c-lightbox').fadeOut();
         }
+    });
+
+    $('.c-lightbox__arrow--next').click(function () {
+        currentIndex = (currentIndex + 1) % $galleryItems.length;
+        const nextSrc = $galleryItems.eq(currentIndex).attr('src');
+        $('.c-lightbox__img').attr('src', nextSrc);
+    });
+
+    $('.c-lightbox__arrow--prev').click(function () {
+        currentIndex = (currentIndex - 1 + $galleryItems.length) % $galleryItems.length;
+        const prevSrc = $galleryItems.eq(currentIndex).attr('src');
+        $('.c-lightbox__img').attr('src', prevSrc);
     });
 });
