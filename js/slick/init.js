63a83dceb1927aef0959220f512a8f1742585efc
nihen99 <nick_henning@web.de>
2025-08-31 11:08:03 +0200

    slick Update und CSS anpassen

--- js/slick/init.js
+++ js/slick/init.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     $('.js-slick-multiple').slick({
         infinite: false,
-        arrows: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        arrows: true,
         prevArrow: '<button type="button" class="slick-prev c-btn">Previous</button>',
         nextArrow: '<button type="button" class="slick-next c-btn">Next</button>',
+        appendArrows: $('.js-slick-multiple__arrows'),
+        dots: true,
         appendDots: $('.js-slick-multiple__dots'),
         responsive: [
             {
