ee7d1a9975cb1c5f6a51610765bc1d4a874873df
nihen99 <nick_henning@web.de>
2025-08-31 10:57:15 +0200

    slick Update

--- js/slick/init.js
+++ js/slick/init.js
@@ -4,7 +4,9 @@ $(document).ready(function () {
         arrows: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-
+        prevArrow: '<button type="button" class="slick-prev c-btn">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next c-btn">Next</button>',
+        appendDots: $('.js-slick-multiple__dots'),
         responsive: [
             {
                 breakpoint: 600,
