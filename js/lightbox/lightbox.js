6bc50f9fef8639f59761378e91ba0ef9d045726c
nihen99 <nick_henning@web.de>
2025-08-31 15:11:13 +0200

    Lightbox optimieren, Tastatursteuerung ermöglichen

--- js/lightbox/lightbox.js
+++ js/lightbox/lightbox.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
     const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
     let currentIndex = -1;
 
-    $galleryItems.click(function () {
-        currentIndex = $galleryItems.index(this);
-        const imgSrc = $(this).attr('src');
+    function openLightbox(index) {
+        currentIndex = index;
+        const imgSrc = $galleryItems.eq(index).attr('src');
         $('.c-lightbox__img').attr('src', imgSrc);
         $('.c-lightbox').fadeIn().css("display", "flex");
+    }
+
+    $galleryItems.on('click', function () {
+        openLightbox($galleryItems.index(this));
+    });
+
+    $galleryItems.on('keydown', function (e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            openLightbox($galleryItems.index(this));
+        }
     });
 
     $('.c-lightbox__close, .c-lightbox').click(function (e) {
@@ -17,13 +27,11 @@ $(document).ready(function () {
 
     $('.c-lightbox__arrow--next').click(function () {
         currentIndex = (currentIndex + 1) % $galleryItems.length;
-        const nextSrc = $galleryItems.eq(currentIndex).attr('src');
-        $('.c-lightbox__img').attr('src', nextSrc);
+        openLightbox(currentIndex);
     });
 
     $('.c-lightbox__arrow--prev').click(function () {
         currentIndex = (currentIndex - 1 + $galleryItems.length) % $galleryItems.length;
-        const prevSrc = $galleryItems.eq(currentIndex).attr('src');
-        $('.c-lightbox__img').attr('src', prevSrc);
+        openLightbox(currentIndex);
     });
 });
