3636d2473fd84709d95f270983ce6baadea3c99e
nihen99 <nick_henning@web.de>
2025-08-31 13:50:31 +0200

    Lightox implementieren

---
+++ js/lightbox/lightbox.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        $('.js-lightbox-trigger').click(function () {
+            const imgSrc = $(this).attr('src');
+            $('.c-lightbox__img').attr('src', imgSrc);
+            $('.c-lightbox').fadeIn();
+        });
+
+        $('.c-lightbox__close, .c-lightbox').click(function (e) {
+            // Nur schließen, wenn nicht direkt aufs Bild geklickt wurde
+            if (!$(e.target).is('.c-lightbox__img')) {
+                $('.c-lightbox').fadeOut();
+            }
+        });
+    });
+
+});
