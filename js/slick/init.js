24f5d77b38ddd0d5ad6b31637125d465cda19523
nihen99 <nick_henning@web.de>
2025-08-31 11:59:40 +0200

    Slider Init anpassen

--- js/slick/init.js
+++ js/slick/init.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
 
         // Nav
         arrows: true,
-        prevArrow: '<button type="button" class="slick-prev c-btn">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next c-btn">Next</button>',
+        prevArrow: '<button title="Previous" type="button" class="slick-prev c-btn"><svg width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/></svg></button>',
+        nextArrow: '<button title="Next" type="button" class="slick-next c-btn"><svg width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/></svg></button>',
         appendArrows: $('.js-slick-multiple__arrows'),
         dots: true,
         appendDots: $('.js-slick-multiple__dots'),
