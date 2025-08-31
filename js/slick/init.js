e0fb941e1b63597ab60964619beee291c90e42b2
nihen99 <nick_henning@web.de>
2025-08-31 11:19:42 +0200

    slick init anpassen

--- js/slick/init.js
+++ js/slick/init.js
@@ -1,26 +1,35 @@
 $(document).ready(function () {
     $('.js-slick-multiple').slick({
+        // Common
         infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
+        autoplay: false,
+        vertical: false,
+        verticalSwiping: false,
+
+        // Nav
         arrows: true,
         prevArrow: '<button type="button" class="slick-prev c-btn">Previous</button>',
         nextArrow: '<button type="button" class="slick-next c-btn">Next</button>',
         appendArrows: $('.js-slick-multiple__arrows'),
         dots: true,
         appendDots: $('.js-slick-multiple__dots'),
+
+        // Layout
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true,
         responsive: [
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 1000,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                 }
             }
