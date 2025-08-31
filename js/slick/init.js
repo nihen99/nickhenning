9964d259b50cc43351b7b18acacee812c4c4972e
nihen99 <nick_henning@web.de>
2025-08-31 10:49:23 +0200

    slick Init und index.html anpassen

--- js/slick/init.js
+++ js/slick/init.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
     $('.js-slick-multiple').slick({
         infinite: false,
+        arrows: false,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 });
