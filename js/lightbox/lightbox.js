3d4ac51fe3f02f6cef5fbf796f1be946bd736160
nihen99 <nick_henning@web.de>
2025-09-01 12:44:08 +0200

    Lightbox Preloading entfernen

--- js/lightbox/lightbox.js
+++ js/lightbox/lightbox.js
@@ -1,31 +1,24 @@
 $(document).ready(function () {
-    const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
     let currentIndex = -1;
 
-    function preloadImage(src) {
-        const img = new Image();
-        img.src = src;
-    }
-
     function openLightbox(index) {
+        const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
         currentIndex = index;
         const imgSrc = $galleryItems.eq(index).attr('src');
         $('.c-lightbox__img').attr('src', imgSrc);
         $('.c-lightbox').fadeIn().css("display", "flex");
-
-        // Preload next and previous images
-        const nextIndex = (index + 1) % $galleryItems.length;
-        const prevIndex = (index - 1 + $galleryItems.length) % $galleryItems.length;
-        preloadImage($galleryItems.eq(nextIndex).attr('src'));
-        preloadImage($galleryItems.eq(prevIndex).attr('src'));
     }
 
-    $galleryItems.on('click', function () {
+    // Delegated click event
+    $('.c-gallery__grid').on('click', '.js-lightbox-trigger', function () {
+        const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
         openLightbox($galleryItems.index(this));
     });
 
-    $galleryItems.on('keydown', function (e) {
+    // Delegated keydown event
+    $('.c-gallery__grid').on('keydown', '.js-lightbox-trigger', function (e) {
         if (e.key === 'Enter' || e.keyCode === 13) {
+            const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
             openLightbox($galleryItems.index(this));
         }
     });
@@ -37,11 +30,13 @@ $(document).ready(function () {
     });
 
     $('.c-lightbox__arrow--next').click(function () {
+        const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
         currentIndex = (currentIndex + 1) % $galleryItems.length;
         openLightbox(currentIndex);
     });
 
     $('.c-lightbox__arrow--prev').click(function () {
+        const $galleryItems = $('.c-gallery__grid .js-lightbox-trigger');
         currentIndex = (currentIndex - 1 + $galleryItems.length) % $galleryItems.length;
         openLightbox(currentIndex);
     });
