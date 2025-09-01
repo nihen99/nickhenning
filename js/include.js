fdffa99f64450aa3ce4a22901e343cdbfdf6213c
nihen99 <nick_henning@web.de>
2025-09-01 14:48:41 +0200

    JS als inline script einbinden

--- js/include.js
+++ js/include.js
@@ -1,12 +1,2 @@
 $(document).ready(function () {
-  const currentPage = window.location.href;
-
-  $('.c-nav__list__item').each(function () {
-    const linkHref = $(this).attr('href');
-    const absoluteHref = new URL(linkHref, window.location.origin).href;
-
-    if (currentPage === absoluteHref) {
-      $(this).addClass('c-nav__list__item--current');
-    }
-  });
 });
