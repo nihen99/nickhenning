ef6408e3289972cad765bc2ddea6f4b38163f5e6
nihen99 <nick_henning@web.de>
2025-09-01 18:13:48 +0200

    Cookie Consent anpassen

--- js/cookieconsent/cookie-consent.js
+++ js/cookieconsent/cookie-consent.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const embedHTML = `
       <iframe title="Spotify Iframe of Artist Nick Henning" data-testid="embed-iframe" style="border-radius:12px"
         src="https://open.spotify.com/embed/artist/3NOae9Khh92LtyDsu8UFYL?utm_source=generator&theme=0"
-        width="100%" height="350" frameBorder="0" allowfullscreen=""
+        width="100%" height="500" frameBorder="0" allowfullscreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"></iframe>
     `;
 
     function renderDeclineMessage() {
       consentContainer.innerHTML = `
-        <div class="c-cooie-consent__content">
+        <div class="c-cookie-consent__content">
             <h3 class="c-heading c-heading--h3">Einbindung blockiert</h3>
             <p class="c-text">Du hast die Spotify Einbindung abgeleht, dadurch hast du das Tarcking von Spotify blockiert. Du möchtest die Einbindung aktivieren?</p>
             <button class="c-btn c-btn--primary js-cookie-consent-accept">Aktivieren</button>
