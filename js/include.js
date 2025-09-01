b5e309c13835b533412f59701c1f9ed000fbf235
nihen99 <nick_henning@web.de>
2025-09-01 16:10:48 +0200

    Loading anpassen

--- js/include.js
+++ js/include.js
@@ -1,13 +1,3 @@
 $(document).ready(function () {
-    $('a').on('click', function (e) {
-    e.preventDefault();
-    $('.c-loading').removeClass('c-loading--in');
-    $('.c-loading').addClass('c-loading--out');
-
-    // Nach kurzer Verzögerung zur Zielseite navigieren
-    const link = $(this).attr('href');
-    setTimeout(() => {
-        window.location.href = link;
-    }, 1000); // Dauer der Animation
-    });
+    
 });
