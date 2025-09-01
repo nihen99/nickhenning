5c8a233c0982a2cebbf8dfda2c7220cb5ba0fff3
nihen99 <nick_henning@web.de>
2025-09-01 18:01:05 +0200

    Cookie Consent anpassen

--- js/cookieconsent/cookie-consent.js
+++ js/cookieconsent/cookie-consent.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const embedHTML = `
       <iframe data-testid="embed-iframe" style="border-radius:12px"
         src="https://open.spotify.com/embed/artist/3NOae9Khh92LtyDsu8UFYL?utm_source=generator&theme=0"
-        width="100%" height="352" frameBorder="0" allowfullscreen=""
+        width="100%" height="500" frameBorder="0" allowfullscreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"></iframe>
     `;
 
     function renderDeclineMessage() {
       consentContainer.innerHTML = `
-        <p>Spotify-Embed wurde deaktiviert.</p>
-        <button class="c-btn js-cookie-consent-accept">Doch erlauben</button>
+        <p class="c-text">Spotify Einbindung wurde deaktiviert.</p>
+        <button class="c-btn c-btn--primary js-cookie-consent-accept">Doch erlauben</button>
       `;
       localStorage.setItem('spotifyConsent', 'declined');
 
