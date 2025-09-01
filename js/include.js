fedae418d0a6672709591958804d8341ff3c3267
nihen99 <nick_henning@web.de>
2025-09-01 14:37:02 +0200

    Nav JS Funktion für Current Page implementieren

--- js/include.js
+++ js/include.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    
+    const currentPath = window.location.pathname.split('/').pop();
+
+    $('.c-nav__list__item').each(function () {
+        const linkPath = $(this).attr('href');
+        if (linkPath === currentPath) {
+            $(this).addClass('c-nav__list__item--current');
+        }
+    });
 });
